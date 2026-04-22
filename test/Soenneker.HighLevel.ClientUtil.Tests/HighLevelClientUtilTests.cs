4654d136751d4370a82ab05b17d2c0af5d3ce62b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:17 -0500

    TUnit conversion

--- test/Soenneker.HighLevel.ClientUtil.Tests/HighLevelClientUtilTests.cs
+++ test/Soenneker.HighLevel.ClientUtil.Tests/HighLevelClientUtilTests.cs
@@ -1,20 +1,19 @@
-﻿using Soenneker.HighLevel.ClientUtil.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.HighLevel.ClientUtil.Abstract;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.HighLevel.ClientUtil.Tests;
 
-[Collection("Collection")]
-public sealed class HighLevelClientUtilTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class HighLevelClientUtilTests : HostedUnitTest
 {
     private readonly IHighLevelClientUtil _openapiclientutil;
 
-    public HighLevelClientUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public HighLevelClientUtilTests(Host host) : base(host)
     {
         _openapiclientutil = Resolve<IHighLevelClientUtil>(true);
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
